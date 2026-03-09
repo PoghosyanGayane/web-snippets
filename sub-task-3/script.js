f311c9e47fafe5eba710f99927ed040835d1a189
Gayane <gayane.poghosyan04@gmail.com>
2026-03-09 11:22:42 +0400

    removed what is not needed

--- sub-task-3/script.js
+++ sub-task-3/script.js
@@ -1,8 +1,8 @@
 "use strict";
-// SUB-TASK 11
-// Create a signup form that validates: 
-// email format, password length >= 8, and required fields.
-// Show inline error messages near fields
+// sub-task 2
+// JavaScript validation logic
+// No alert() for error
+
 const ageInput = document.getElementById("age");
 
 ageInput.addEventListener("input", () => {
@@ -14,27 +14,3 @@ ageInput.addEventListener("input", () => {
         ageInput.setCustomValidity("");
     }
 });
-
-// SUB-TASK 12
-// Add error handling to one of your tasks: 
-// if a required DOM element is missing, show a
-// friendly message in the UI and log details to console.
-function customError(message){
-    this.message = message;
-    this.title = "Custom Error";
-    this.stack = `${this.title} ${this.message}`;
-}
-
-
-const fineBtn = document.querySelector("#ok")
-
-try {
-    if (fineBtn.disabled) {
-        throw new customError("you can not make everything fine");
-    }
-} catch(err) {
-    console.error(err.stack);
-    document.getElementById("message").textContent = err.message;
-} finally {
-    // fineBtn.disabled = false;
-}
